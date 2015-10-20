89853defd2acd4073b92a21fc9e22945a33abd80
Christer Eriksson <christer.j.eriksson@gmail.com>
2015-10-20 22:06:22 +0200

    Modifide cli to take arguments to select operation

--- gocd-cli.go
+++ gocd-cli.go
@@ -12,8 +12,6 @@ import (
 
 var log = logging.MustGetLogger("example")
 
-var HOST = "http://localhost:8153"
-
 //Selection is the basis of whcih pipeline/stage and Job to operate on
 type Selection struct {
 	Pipeline string
@@ -25,6 +23,9 @@ type Selection struct {
 func main() {
 	var argHost string
 	flag.StringVar(&argHost, "host", "http://localhost:8153/go/", "Set hostname")
+
+	var argPipelineGroup string
+	flag.StringVar(&argPipelineGroup, "pipeline-group", "", "Specify name of a Pipeline group")
 
 	var argPipeline string
 	flag.StringVar(&argPipeline, "pipeline", "", "Specify name of Pipeline")
@@ -52,61 +53,60 @@ func main() {
 
 	var configPath string
 	flag.StringVar(&configPath, "config", "", "Path to configuration file")
+
 	flag.Parse()
 
-	//coordinate := gocdapi.Coordinate{PipelineName: "AlaskaMultiNodeLassiBlue", PipelineCount: 4, StageName: "BootStrap", StageCount: 1, JobName: "BootStrap"}
-
-	coordinate := gocdapi.Coordinate{PipelineName: argPipeline, PipelineCount: argPipelineCount, StageName: argStage, StageCount: argStageCount, JobName: argJob, FilePath: argPath}
+	coordinate := gocdapi.Coordinate{PipelineGroup: argPipelineGroup, PipelineName: argPipeline, PipelineCount: argPipelineCount, StageName: argStage, StageCount: argStageCount, JobName: argJob, FilePath: argPath}
 
 	fmt.Println("host: " + argHost)
 
 	client := gocdapi.NewClient(&http.Client{})
 	client.BaseURL, _ = url.Parse(argHost)
+
 	if argUserName != "" && argPassword != "" {
 		client.UserName = &argUserName
 		client.Password = &argPassword
 	}
 
-	fmt.Println("Agent: GETALL")
-	agents, _, _ := client.Agents.GetAll()
-	fmt.Println(agents)
-	fmt.Println("Agent: GET UUID: " + agents.Embedded.Agents[1].UUID)
-	agent, _, _ := client.Agents.Get(agents.Embedded.Agents[1].UUID)
-	fmt.Println(agent)
-	fmt.Println(agent.Resources)
-	fmt.Println("PipelineGroup: GET")
-	pipelinegroups, _, _ := client.PipelineGroup.Get()
-	fmt.Println(pipelinegroups)
+	switch flag.Arg(0) {
+	case "group":
+		pipelinegroups, _, _ := client.PipelineGroup.Get()
+		fmt.Println(pipelinegroups)
+	case "agent":
+		agents, _, _ := client.Agents.GetAll()
+		fmt.Println(agents)
+		/*
+			agent, _, _ := client.Agents.Get(agents.Embedded.Agents[1].UUID)
+			fmt.Println(agent)
+			fmt.Println(agent.Resources)
+		*/
+	case "user":
+		users, _, _ := client.Users.GetAll()
+		fmt.Println(users)
+	case "artifacts":
+		artifacts, _, _ := client.Artifacts.GetAllArtifacts(coordinate)
+		fmt.Println(artifacts)
+		/*
+			artifact, _, _ := client.Artifacts.GetArtifactFile(coordinate)
+			fmt.Println(artifact)
+			coordinate.FilePath = "cruise-output"
+			directory, _, _ := client.Artifacts.GetArtifactDirectory(coordinate)
+			fmt.Println(directory)
+		*/
+	case "pipeline":
+		switch flag.Arg(1) {
+		case "instance":
+			pipelineinstance, _, _ := client.Pipelines.GetInstance(coordinate)
+			fmt.Println(pipelineinstance)
+		case "history":
 
-	fmt.Println("Users: GETALL")
-	users, _, _ := client.Users.GetAll()
-	fmt.Println(users)
-	/*
-		userName := "patrik.berglund"
-		fmt.Println("Users: GET: " + userName)
-		user, _, _ := client.Users.Get(userName)
-		fmt.Println(user)
-	*/
+			pipelinehistory, _, _ := client.Pipelines.GetHistory(coordinate)
+			fmt.Println(pipelinehistory)
+		case "status":
+			pipelinestatus, _, _ := client.Pipelines.GetStatus(coordinate)
+			fmt.Println(pipelinestatus)
+		}
 
-	fmt.Println("Artifacts: GETALL")
-	//curl 'http://localhost:8153/go/files/AlaskaMultiNodeLassiBlue/4/BootStrap/1/BootStrap.json'       -u 'christer.eriksson:p4rqBE24'
+	}
 
-	artifacts, _, _ := client.Artifacts.GetAllArtifacts(coordinate)
-	fmt.Println(artifacts)
-
-	artifact, _, _ := client.Artifacts.GetArtifactFile(coordinate)
-	fmt.Println(artifact)
-
-	coordinate.FilePath = "cruise-output"
-	directory, _, _ := client.Artifacts.GetArtifactDirectory(coordinate)
-	fmt.Println(directory)
-
-	pipelineinstance, _, _ := client.Pipelines.GetInstance(coordinate)
-	fmt.Println(pipelineinstance)
-
-	pipelinehistory, _, _ := client.Pipelines.GetHistory(coordinate)
-	fmt.Println(pipelinehistory)
-
-	pipelinestatus, _, _ := client.Pipelines.GetStatus(coordinate)
-	fmt.Println(pipelinestatus)
 }
